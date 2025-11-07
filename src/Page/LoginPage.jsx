410269e9f7c60e6fe81cbe6aba55da4f61afe5a9
jeelrajpara18 <jeelrajpara18@gmail.com>
2025-11-07 17:00:57 +0530

    Base folder change

--- src/Page/LoginPage.jsx
+++ src/Page/LoginPage.jsx
@@ -4,14 +4,14 @@ import logo from "../assets/logo.svg";
 import iconText from "../assets/logo-text.svg";
 import { loginUser } from "../Api/authApi";
 import { toast } from "react-toastify";
-import BaseLoader from "../Component/Base/BaseLoader";
 import { ROUTES } from "../Routes/RouteConstants";
 import { ArrowLeft, Underline } from "../assets/svg";
 import { loginConstants } from "../common/constants/loginConstants";
-import BaseLogin from "../Component/BASE/BaseLogin";
+import BaseLogin from "../Component/Base/BaseLogin";
 import { errorMessages, regex } from "../common/Validations";
 import * as Yup from "yup";
 import { fieldNames } from "../common/constants/formFields";
+import BaseLoader from "../Component/Base/BaseLoader";
 
 export default function LoginPage() {
   const [loading, setLoading] = useState(false);
